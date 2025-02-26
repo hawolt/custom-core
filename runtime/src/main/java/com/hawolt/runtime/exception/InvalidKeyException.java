724206136bf103f0a779f3b542040159acfdbfb9
hawolt <45050975+hawolt@users.noreply.github.com>
2025-02-26 14:22:53 +0100

    remove files from runtime project

--- runtime/src/main/java/com/hawolt/runtime/exception/InvalidKeyException.java
+++
@@ -1,8 +0,0 @@
-package com.hawolt.runtime.exception;
-
-public class InvalidKeyException extends Exception {
-
-    public InvalidKeyException(String key) {
-        super("Specified key [" + key + "] is not present");
-    }
-}
--- runtime/src/main/java/com/hawolt/runtime/exception/InvalidObjectException.java
+++
@@ -1,8 +0,0 @@
-package com.hawolt.runtime.exception;
-
-public class InvalidObjectException extends RuntimeException {
-
-    public InvalidObjectException(Object o) {
-        super("Invalid Object: " + o.toString());
-    }
-}
